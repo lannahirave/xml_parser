e1c90dde6d46c96e4c2cb2472a92831e5ff38dbd
Valeriy <nikitinrus404@gmail.com>
2021-11-14 23:11:40 +0200

    tests stable ver.

--- tests/test_1.py
+++ tests/test_1.py
@@ -20,7 +20,7 @@ class bcolors:
 file = 'file.xml'
 answers = ['Розклад занять для студентів ФКНК', 'Розклад', 'Електронний', 'Фінальна версія', ['Юрій Головач', 'Євген Пархоменко', 'Ярослав Великий'], 'Вільні умови', '1524'],\
 ['Розклад занять для викладачів ФКНК', 'Розклад', 'Електронний', 'Фінальна версія', ['Олена Висоцька'], 'Вільні умови', '123'],\
-['Відомість студентів', 'Відомість з відмітками', 'Електронний', 'Фінальна версія', ['Кіріл Шевчук', 'Антон Работніков', 'Максим Гнідченко'], 'Для викладачів', '110'],\
+['Відомість студентів', 'Відомість з відмітками', 'Фізичний', 'Фінальна версія', ['Кіріл Шевчук', 'Антон Работніков', 'Максим Гнідченко'], 'Для викладачів', '110'],\
 ['Тритон', 'Помічник студента', 'Електронний', 'Редагуєма версія', ['Наполеон', 'Деканат №1', 'Деканат №3'], 'Для студентів', '31253']
 
 class Tests(unittest.TestCase):
@@ -30,10 +30,10 @@ class Tests(unittest.TestCase):
         """
         #print('----------------------------------------------------------------------')
         parser = strategy.SAX_parser()
-        parser.parse()
+        parser.parse(file)
         answer = parser.get_list()
         for i in range(len(answer)):
-            self.assertEqual(answer[i].info(), answers[i])
+            self.assertEqual(answer[i].properties(), answers[i])
         print(bcolors.OKGREEN + "SAX SUCCESS." + bcolors.ENDC)
         
     def test_fast(self):
@@ -42,10 +42,10 @@ class Tests(unittest.TestCase):
         """
         #print('----------------------------------------------------------------------')
         parser = strategy.FAST_parser()
-        parser.parse()
+        parser.parse(file)
         answer = parser.get_list()
         for i in range(len(answer)):
-            self.assertEqual(answer[i].info(), answers[i])
+            self.assertEqual(answer[i].properties(), answers[i])
         print(bcolors.OKGREEN + "FAST SUCCESS." + bcolors.ENDC)
     
     def test_dom(self):
@@ -54,10 +54,10 @@ class Tests(unittest.TestCase):
         """
         #print('----------------------------------------------------------------------')
         parser = strategy.DOM_parser()
-        parser.parse()
+        parser.parse(file)
         answer = parser.get_list()
         for i in range(len(answer)):
-            self.assertEqual(answer[i].info(), answers[i])
+            self.assertEqual(answer[i].properties(), answers[i])
         print(bcolors.OKGREEN + "DOM SUCCESS." + bcolors.ENDC)
  
 
