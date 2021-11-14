2bf8db540c6a82113472f786c2d534c4fa5ee6b6
Valeriy <nikitinrus404@gmail.com>
2021-11-14 23:12:41 +0200

    remake info(): now returns str 'text'

--- service.py
+++ service.py
@@ -4,9 +4,8 @@
 class Service:
     def __init__(self, args):
         self.names = ["Назва", "Анотація", "Тип", "Версія", "Автор", "Умови", "Зареєстровані"]
-        
         try:
-            self.properties = args
+            self._properties = args
             self.title = args[0]
             self.annotation = args[1]
             self.type = args[2]
@@ -17,9 +16,22 @@ class Service:
         except Exception as exp:
             print(exp)
         
-    def info(self):
-        return [self.title, self.annotation, self.type, self.version, self.authors,\
-            self.terms, self.registered]
+    def info(self) -> str:
+        """[summary]
+
+        Returns:
+            str: [information about properties]
+        """
+        text = ''
+        for i in range(len(self.names)):
+            if self.names[i].lower() == "автор":
+               for author in self.authors:
+                  text += self.names[i] + ": " + author + '\n'  
+            else:
+                text += self.names[i] + ": " + self._properties[i] + '\n'
+        text += '-'*10 + '\n'
+        return text
+    
+    def properties(self) -> list:
+        return self._properties
         
-    def set_from_dict(self):
-        pass
